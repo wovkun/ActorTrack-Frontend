47f96d976c9cdcd53a7e39ce3d39ca3168ae2ae6
wovkun <volpastern@gmail.com>
2025-02-24 02:03:30 +0200

    [ATF-02] server communication/routing for auth

---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { JwtService } from '../services/jwt.service';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(JwtService).getToken();
+
+  const request = req.clone({
+    setHeaders: {
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+  });
+  return next(request);
+};

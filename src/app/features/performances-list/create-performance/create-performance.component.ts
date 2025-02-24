c0680a7f0be758de3c391bdea007e6d060d55955
wovkun <volpastern@gmail.com>
2025-02-24 09:13:44 +0200

    [ATF-03] performance list

--- src/app/features/performances-list/create-performance/create-performance.component.ts
+++ src/app/features/performances-list/create-performance/create-performance.component.ts
@@ -1,10 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PerformanceService } from '../../../core/services/performance.service';
+import { Performance } from '../../../shared/models/performance.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-performance',
+  standalone: true,
   templateUrl: './create-performance.component.html',
-  styleUrl: './create-performance.component.scss'
+  styleUrls: ['./create-performance.component.scss'],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
 })
-export class CreatePerformanceComponent {
+export class CreatePerformanceComponent implements OnInit {
+  performanceForm: FormGroup;
+  formSubmitted = false;
+  isEditMode: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private performanceService: PerformanceService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute
+  ) {
+    this.performanceForm = this.fb.group({
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+      year: [
+        '',
+        [
+          Validators.required,
+          Validators.min(1900),
+          Validators.max(new Date().getFullYear()),
+        ],
+      ],
+      budget: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const performanceId = params['performanceId'];
+      if (performanceId) {
+        this.isEditMode = true;
+        this.performanceService.getPerformanceById(performanceId).subscribe(
+          (performance: Performance) => {
+            this.performanceForm.patchValue(performance);
+          },
+          (error) => {
+            this.snackBar.open('Error fetching actor data.', 'Close', {
+              duration: 3000,
+            });
+            console.error('Error fetching actor data:', error);
+          }
+        );
+      }
+    });
+  }
+
+  get formControls() {
+    return this.performanceForm.controls;
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+
+    if (this.performanceForm.invalid) {
+      return;
+    }
+
+    const performanceData: Performance = this.performanceForm.value;
+
+    if (this.isEditMode) {
+      this.route.params.subscribe((params) => {
+        const performanceId = params['performanceId'];
+
+        this.performanceService
+          .updatePerformance(performanceId, performanceData)
+          .subscribe(
+            (response) => {
+              this.snackBar.open('Performance updated successfully!', 'Close', {
+                duration: 3000,
+              });
+              this.router.navigate(['/home']);
+            },
+            (error) => {
+              this.snackBar.open(
+                'Failed to update performance. Please try again.',
+                'Close',
+                { duration: 3000 }
+              );
+              console.error('Error updating performance:', error);
+            }
+          );
+      });
+    } else {
+      this.performanceService.createPerformance(performanceData).subscribe(
+        (response) => {
+          this.snackBar.open('Performance created successfully!', 'Close', {
+            duration: 3000,
+          });
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          this.snackBar.open(
+            'Failed to create performance. Please try again.',
+            'Close',
+            { duration: 3000 }
+          );
+          console.error('Error creating performance:', error);
+        }
+      );
+    }
+  }
+
+  onGoBack() {
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/features/performances-list/performances-list.component.ts
+++ src/app/features/performances-list/performances-list.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PerformanceService } from '../../core/services/performance.service';
+import { Performance } from '../../shared/models/performance.model';
+import { Router } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-performances-list',
   templateUrl: './performances-list.component.html',
-  styleUrl: './performances-list.component.scss'
+  styleUrls: ['./performances-list.component.scss'],
+  imports: [MatTableModule, MatButtonModule, RouterLink],
+  standalone: true,
 })
-export class PerformancesListComponent {
+export class PerformancesListComponent implements OnInit {
+  displayedColumns: string[] = ['title', 'year', 'budget', 'actions'];
+  dataSource = new MatTableDataSource<Performance>();
 
+  constructor(
+    private performanceService: PerformanceService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadPerformances();
+  }
+
+  loadPerformances(): void {
+    this.performanceService.getPerformances().subscribe((performances) => {
+      this.dataSource.data = performances;
+    });
+  }
+
+  createPerformance(): void {
+    this.router.navigate(['/create-performance']);
+  }
+
+  editPerformance(performanceId: string): void {
+    this.router.navigate([`/create-performance/${performanceId}`]);
+  }
+
+  findActors(performanceId: string, performanceTitle: string) {
+    this.router.navigate(['/create-contract'], {
+      queryParams: { performanceId, performanceTitle },
+    });
+  }
+
+  deletePerformance(performanceId: string): void {
+    this.performanceService.deletePerformance(performanceId).subscribe(() => {
+      this.loadPerformances();
+    });
+  }
 }

00d0458f3f6898e73334091a6561752580652fc5
wovkun <volpastern@gmail.com>
2025-02-24 09:13:58 +0200

    [ATF-03] header/home

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { RouterLink } from '@angular/router';
+import {
+  ActiveComponentService,
+  ActiveComponentType,
+} from '../../../core/services/routing.service';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  imports: [RouterLink],
 })
 export class HeaderComponent {
+  constructor(
+    private authService: AuthService,
+    private activeComponentService: ActiveComponentService
+  ) {}
 
+  onSelectComponent(component: ActiveComponentType) {
+    this.activeComponentService.setActiveComponent(component);
+  }
+
+  public logout(): void {
+    this.authService.logout();
+    
+  }
 }
